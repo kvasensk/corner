63853189d759bb65971bd92835ee4a33487165b4
Stanislav <stanisalv.khannolainen@yandex.ru>
2025-07-23 10:31:35 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const repo = 'corner';
 
-const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? `/${repo}` : '',
-  assetPrefix: isGithubPages ? `/${repo}/` : '',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   trailingSlash: true,
   images: {
     unoptimized: true,
